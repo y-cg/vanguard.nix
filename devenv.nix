51eff7e0f85e8cb86ec389861cb0b0b18ed849cd
y-cg <159802291+y-cg@users.noreply.github.com>
2026-03-06 18:28:34 +0800

    ci: simplify package listing using devenv

--- devenv.nix
+++ devenv.nix
@@ -7,8 +7,16 @@
 }:
 
 {
+  scripts = {
+    list-pkgs.exec = ''
+      nix flake show --json --all-systems | \
+        jq -r ".packages.\"$@\" | to_entries[] | .key" | \
+        jq -R -s -c 'split("\n") | map(select(length > 0))'
+    '';
+  };
+
   # https://devenv.sh/packages/
-  packages = [ ];
+  packages = with pkgs; [ jq ];
 
   # https://devenv.sh/languages/
   languages.nix.enable = true;
