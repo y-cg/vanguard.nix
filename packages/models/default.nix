027793f2dd81eebc048ecc534f06de7166b70cc8
y-cg <159802291+y-cg@users.noreply.github.com>
2026-03-27 15:32:30 +0800

    build(models): bump version to 0.11.3

--- packages/models/default.nix
+++ packages/models/default.nix
@@ -7,16 +7,16 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "models";
-  version = "0.11.1";
+  version = "0.11.3";
 
   src = fetchFromGitHub {
     owner = "arimxyer";
     repo = "models";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-M1wBqE6+OXSdNyk6g3UnaARGhZkI4snY8BOIiLXHEss=";
+    hash = "sha256-APLJwwn25XmIm1tfJewqbwTFmUpYPTZEsSdy0FHhdb8=";
   };
 
-  cargoHash = "sha256-VkMj64SHtYZTP8l+Qu1J47iSgh9Ib0dULcIs7AQW52Q=";
+  cargoHash = "sha256-ArIFhwf8+pTP7VzgCDAjtOJjRtVDOqSEjArvMW1ZR1s=";
   doCheck = false;
 
   nativeBuildInputs = with pkgs; [
