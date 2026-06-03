3b0ea96642ce5c085b2cd837ee78c5701eff6d92
y-cg <159802291+y-cg@users.noreply.github.com>
2026-06-03 20:37:16 +0000

    chore(cloudflare-speed-cli): update package

--- packages/cloudflare-speed-cli/default.nix
+++ packages/cloudflare-speed-cli/default.nix
@@ -6,17 +6,17 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "cloudflare-speed-cli";
-  version = "1.0.0";
+  version = "1.0.1";
 
   src = fetchFromGitHub {
     owner = "kavehtehrani";
     repo = "cloudflare-speed-cli";
     rev = "v${version}";
-    hash = "sha256-J3368ZgEopESWJK+/7OhAwHeKHVeORHkfAfOhvahuFc=";
+    hash = "sha256-6XRz5kLPkE0U/MphddKsGUHudET5FANvvDzEsYgfce4=";
   };
 
   doCheck = false;
-  cargoHash = "sha256-Gc0Cwpn9K9m0RSx7TTdYD5gzVvmGBIGk3BwFQD9jDq8=";
+  cargoHash = "sha256-y60+qmvEactXcrCFmGmNlFEY8pL3Ux2X4cd+MLXvbaM=";
 
   meta = {
     description = "CLI for internet speed test via cloudflare";
