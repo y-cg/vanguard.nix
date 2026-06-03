ec4610fe0f75b6d41e35859cc2b38dc53f869ef2
y-cg <159802291+y-cg@users.noreply.github.com>
2026-06-03 23:23:13 +0800

    feat: add vimhjkl package

---
+++ packages/vimhjkl/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  python3Packages,
+  fetchFromGitHub,
+}:
+
+python3Packages.buildPythonApplication (finalAttrs: {
+  pname = "vimhjkl";
+  version = "0.3.0";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "S-Sigdel";
+    repo = "vimhjkl";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-7J26Umj/e2QZdCxib8lcv3+CDYtTIz5CrTg6H7YlbJ0=";
+  };
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail "uv_build>=0.11,<0.12" uv_build
+  '';
+
+  build-system = with python3Packages; [ uv-build ];
+
+  doCheck = false;
+
+  meta = with lib; {
+    description = "Learn Vim from your terminal with spaced repetition";
+    homepage = "https://github.com/S-Sigdel/vimhjkl";
+    changelog = "https://github.com/S-Sigdel/vimhjkl/releases/tag/v${finalAttrs.version}";
+    license = licenses.mit;
+    maintainers = [ ];
+    mainProgram = "vimhjkl";
+  };
+})
