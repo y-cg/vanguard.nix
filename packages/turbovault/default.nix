34c2f093895ab6265d0a089b93544292142e377e
y-cg <159802291+y-cg@users.noreply.github.com>
2026-09-14 11:33:34 +0000

    chore(turbovault): update package

--- packages/turbovault/default.nix
+++ packages/turbovault/default.nix
@@ -9,16 +9,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "turbovault";
-  version = "2.0.0";
+  version = "2.1.0";
 
   src = fetchFromGitHub {
     owner = "Epistates";
     repo = "turbovault";
     rev = "v${version}";
-    hash = "sha256-1QBdvNtJdIaajq9d/bbUSQ74fWLpZ20ROtKmNiGIezo=";
+    hash = "sha256-HHcR0zcNVn0h+jU0PNQ+CmQeRzMjvnmkJ0Qim3eoNt8=";
   };
 
-  cargoHash = "sha256-ThTwGVsNp65B7CAbp7mgwy4APAXG/3TZ0JZTJRe0MDM=";
+  cargoHash = "sha256-cdkGSzqEUyT0Dx3MGzqADxeM5n7q35xaetIkJUeATGo=";
 
   nativeBuildInputs = [ pkg-config ];
 
