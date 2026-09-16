10d9cb46b6192191119f4eadb11981498aa8ab3a
y-cg <159802291+y-cg@users.noreply.github.com>
2026-09-16 10:43:18 +0000

    chore(plannotator): update package

--- packages/plannotator/default.nix
+++ packages/plannotator/default.nix
@@ -14,23 +14,23 @@
 }:
 
 let
-  version = "0.27.14";
+  version = "0.27.15";
   sources = {
     aarch64-darwin = {
       arch = "darwin-arm64";
-      hash = "sha256-Hp9w9FTTkwKPLW/hLaiqq5MbNZH9NBqtNCgP8ZgepNA=";
+      hash = "sha256-8z4RRFH9GWv/xGqiF0kvxyT+FYKM6GAHYJz+vAitUwQ=";
     };
     x86_64-darwin = {
       arch = "darwin-x64";
-      hash = "sha256-jGyTq+4m6BSvS50ddyDKk7Ez64JLis2YTd2cMUHGFHk=";
+      hash = "sha256-8GCS8OMWrefRFeY06drswJQyU0T+d0GWGTQKGGmuwkg=";
     };
     aarch64-linux = {
       arch = "linux-arm64";
-      hash = "sha256-XrvW0VEwwmM7+pspUxIFfoJ3lUqY9ALbZesSSzHnCE0=";
+      hash = "sha256-GGtP7snEf/8gOKDOrwY09L9s4k9qtteAw+9CLCS01A8=";
     };
     x86_64-linux = {
       arch = "linux-x64";
-      hash = "sha256-ldslSssqmW6w2DKJq7uR384rDd7jeLY3UgXj6CUajbA=";
+      hash = "sha256-79sz9OCqFNZufwNYdzoRzEb1tfwiVwnW6sTZ6Nllo1U=";
     };
   };
   source =
